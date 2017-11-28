c7bc2e0f379b2c16fd7c15df256c9d88490bb3cf
illicit <illicit@cavendersoftworks.com>
2017-11-27 18:45:35 -0800

    Added GSON library and changed to store persistent JSON instead of raw text

--- JSONHelper.java
+++ JSONHelper.java
@@ -1,15 +1,20 @@
 package com.cavendersoftworks.persistentstoragepractice;
 
 import android.content.Context;
+import android.os.Environment;
 import android.util.Log;
 
 import com.google.gson.Gson;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
 
 public class JSONHelper {
 
-    public static String exportToJSON(Context context, ArrayList<DataItem> dataItems) {
+    public static String exportToJSON(ArrayList<DataItem> dataItems) {
         DataItems da = new DataItems();
         da.dataItems = dataItems;
 
@@ -17,6 +22,31 @@ public class JSONHelper {
         String jsonString = gson.toJson(da);
         Log.i("JSONHelper", "Export to JSON: " + jsonString);
         return jsonString;
+    }
+
+    public static ArrayList<DataItem> importFromJSON(Context context) {
+        FileReader reader = null;
+
+        try {
+            File file = new File(Environment.getExternalStorageDirectory(), MainActivity.FILENAME);
+            reader = new FileReader(file);
+            Gson gson = new Gson();
+            DataItems da  = gson.fromJson(reader, DataItems.class);
+            return da.getDataItems();
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } finally {
+            if (reader != null) {
+                try {
+                    reader.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
+        return null;
     }
 
     static class DataItems {
--- MainActivity.java
+++ MainActivity.java
@@ -11,12 +11,9 @@ import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
@@ -43,9 +40,15 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+    private void checkReadPermission() {
+        if ((ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE))  != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},REQUEST_WRITE_PERMISSION);
+        }
+    }
+
     public void onCreateButtonClick(View view) {
         checkWritePermission();
-        String result = JSONHelper.exportToJSON(this, dataItems);
+        String result = JSONHelper.exportToJSON(dataItems);
         FileOutputStream fos = null;
         File file = new File(Environment.getExternalStorageDirectory(), FILENAME);
 
@@ -59,18 +62,17 @@ public class MainActivity extends AppCompatActivity {
         } finally {
             try {
                 fos.close();
-            } catch (IOException e) {
+            } catch (Exception e) {
                 e.printStackTrace();
             }
         }
     }
 
     public void onDeleteButtonClick(View view) {
-        File file = new File(getFilesDir(), FILENAME);
+        File file = new File(Environment.getExternalStorageDirectory(), FILENAME);
 
         if(file.exists()) {
-            deleteFile(FILENAME);
-            Toast.makeText(this, "File " + FILENAME + " deleted successfully", Toast.LENGTH_SHORT).show();
+            if(file.delete()) { Toast.makeText(this, "File " + FILENAME + " deleted successfully", Toast.LENGTH_SHORT).show(); }
         } else {
             Toast.makeText(this, "No file was found", Toast.LENGTH_SHORT).show();
         }
@@ -78,35 +80,14 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void onReadButtonClick(View view) {
-        File file = new File(getFilesDir(), FILENAME);
-        FileInputStream fis = null;
-        InputStreamReader isr = null;
-        BufferedReader br = null;
-        String input;
-
-        if(file.exists()) {
-            try {
-                fis = openFileInput(FILENAME);
-                isr = new InputStreamReader(fis);
-                br = new BufferedReader(isr);
-                StringBuilder sb = new StringBuilder();
-                while ((input = br.readLine()) != null) {
-                    sb.append(input).append("\n");
-                }
-                output.setText(sb.toString());
-                Toast.makeText(this, "File read successfully", Toast.LENGTH_SHORT).show();
-            } catch (IOException e) {
-                e.printStackTrace();
-                Toast.makeText(this, "Error: " + e.getMessage(), Toast.LENGTH_LONG).show();
-            } finally {
-                try {
-                    fis.close();
-                    isr.close();
-                    br.close();
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
+        checkReadPermission();
+        StringBuilder sb = new StringBuilder();
+        ArrayList<DataItem> dataItemList = JSONHelper.importFromJSON(this);
+        if(dataItemList != null) {
+            for(DataItem da : dataItemList) {
+                sb.append(da.getUID() + "\n" + da.getName() + "\n" + da.getDesc() + "\n");
             }
+            output.setText(sb.toString());
         } else {
             Toast.makeText(this, "File does not exist", Toast.LENGTH_SHORT).show();
         }
